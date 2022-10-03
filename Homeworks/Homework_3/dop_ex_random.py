124fa686d0f66378daaae49f4dde2e276d9a8b50
Vsevolod <vsevolod.lashin@gmail.com>
2022-10-03 14:26:38 +0300

    Homework 3 added

---
+++ Homeworks/Homework_3/dop_ex_random.py
@@ -0,0 +1,25 @@
+# Реализуйте алгоритм задания случайных чисел без использования 
+# встроенного генератора псевдослучайных чисел.
+
+import time
+
+def random1(n, min, max):
+
+    list1 = []
+    
+    for i in range(1, n + 1):
+       
+        x = round(i * (time.time() * 1000) % (10 ** (len(str(max)) - 1)))
+        
+        while x > max or x < min:
+            x = round(i * (time.time() * 10 ** len(str(max))) % (10 ** (len(str(max)))))
+        
+        if i % 2 == 0 and -x > min:
+            list1.append(-x)
+        else: 
+            list1.append(x)   
+    
+    return list1
+
+result_list = random1(5, -500, 1000)
+print(result_list)
