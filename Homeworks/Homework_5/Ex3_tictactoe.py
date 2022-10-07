701b4752a4ae988b0f241679b84fa0cf6aead006
Vsevolod <vsevolod.lashin@gmail.com>
2022-10-08 01:52:35 +0300

    Homework 5 Ex 3 in progress 2

--- Homeworks/Homework_5/Ex3_tictactoe.py
+++ Homeworks/Homework_5/Ex3_tictactoe.py
@@ -81,9 +81,11 @@ def bot_vs_bot(x):
     check = random.randint(1, 2)
 
     if check == 1:
-        print('Первыми ходят X')
+        check2 = 2
+        print('\nПервым ходит бот 1\n')
     else:
-        print('Первыми ходят O')
+        check2 = 1
+        print('\nПервым ходит бот 2\n')
 
     list_check = ''
     for i in range(1, 10):
@@ -92,35 +94,27 @@ def bot_vs_bot(x):
             while l in list_check:
                 l = str(random.randint(1,9))
             list_check += l
-            print(l)
+            print(f'Ход бота {check}: {l}')
             while x[i].isdigit() == False and x[count] != l:
                 count += 1
             else:
-                if check == 1:
-                    x[count] = 'X'
-                    print(''.join(x))
-                    count = 0
-                else:
-                    x[count] = '0'
-                    print(''.join(x))
-                    count = 0
+                x[count] = 'X'
+                print(''.join(x))
+                count = 0
+               
         else:
             l = str(random.randint(1, 9))
             while l in list_check:
                 l = str(random.randint(1,9))
             list_check += l
-            print(l)
+            print(f'Ход бота {check2}: {l}')
             while x[i].isdigit() == False and x[count] != l:
                 count += 1
             else:
-                if check == 1:
-                    x[count] = 'O'
-                    print(''.join(x))
-                    count = 0
-                else:
-                    x[count] = 'X'
-                    print(''.join(x))
-                    count = 0
+                x[count] = 'O'
+                print(''.join(x))
+                count = 0
+    
     return 'Конец!'
 
 
@@ -144,59 +138,59 @@ def player_vs_bot(x):
     list_check = ''
 
     for i in range(1, 10):
-        if i % 2 != 0 and check == 1:
-            l = str(input('Введите номер четверти(1 - 9): '))
-            while l in list_check or int(l) < 1 or int(l) > 9:
-                l = str(input('\nЭта клетка уже занята или ее нет.\nВведите номер четверти(1 - 9): '))
-            list_check += l
+        if i % 2 != 0: 
+            if check == 1:
+                l = str(input('Введите номер четверти(1 - 9): '))
+                while l in list_check or int(l) < 1 or int(l) > 9:
+                    l = str(input('\nЭта клетка уже занята или ее нет.\nВведите номер четверти(1 - 9): '))
+                list_check += l
 
-            while x[i].isdigit() == False and x[count] != l:
-                count += 1
+                while x[i].isdigit() == False and x[count] != l:
+                    count += 1
+                else:
+                    x[count] = 'X'
+                    print(''.join(x))
+                    count = 0
             else:
-                x[count] = 'X'
-                print(''.join(x))
-                count = 0
+                l = str(random.randint(1, 9))
+                while l in list_check:
+                    l = str(random.randint(1,9))
+                list_check += l
+                print(f'Ход бота: {l}')
+                while x[i].isdigit() == False and x[count] != l:
+                    count += 1
+                else:
+                    x[count] = 'X'
+                    print(''.join(x))
+                    count = 0
+
+        else:
+            if check == 1:
+                l = str(random.randint(1, 9))
+                while l in list_check:
+                    l = str(random.randint(1,9))
+                list_check += l
+                print(f'Ход бота: {l}')
+                while x[i].isdigit() == False and x[count] != l:
+                    count += 1
+                else:
+                    x[count] = 'O'
+                    print(''.join(x))
+                    count = 0
                 
-        elif i % 2 != 0 and check == 2:
-            l = str(random.randint(1, 9))
-            while l in list_check:
-                l = str(random.randint(1,9))
-            list_check += l
-            print(f'Ход бота: {l}')
-            while x[i].isdigit() == False and x[count] != l:
-                count += 1
             else:
-                x[count] = 'X'
-                print(''.join(x))
-                count = 0
-        
-        elif i % 2 == 0 and check == 1:
-            l = str(random.randint(1, 9))
-            while l in list_check:
-                l = str(random.randint(1,9))
-            list_check += l
-            print(f'Ход бота: {l}')
-            while x[i].isdigit() == False and x[count] != l:
-                count += 1
-            else:
-                x[count] = 'O'
-                print(''.join(x))
-                count = 0
-        
-        elif i % 2 == 0 and check == 2:
-            l = str(input('Введите номер четверти(1 - 9): '))
-            while l in list_check or int(l) < 1 or int(l) > 9:
-                l = str(input('\nЭта клетка уже занята или ее нет.\nВведите номер четверти(1 - 9): '))
-            list_check += l
+                l = str(input('Введите номер четверти(1 - 9): '))
+                while l in list_check or int(l) < 1 or int(l) > 9:
+                    l = str(input('\nЭта клетка уже занята или ее нет.\nВведите номер четверти(1 - 9): '))
+                list_check += l
 
-            while x[i].isdigit() == False and x[count] != l:
-                count += 1
-            else:
-                x[count] = 'O'
-                print(''.join(x))
-                count = 0
+                while x[i].isdigit() == False and x[count] != l:
+                    count += 1
+                else:
+                    x[count] = 'O'
+                    print(''.join(x))
+                    count = 0
                 
-
     return 'Конец!'
 
 
@@ -209,12 +203,12 @@ x = \
 | 7 | 8 | 9 |
  -----------'''
 
-# game_of_two = two_players(x)
-# print(game_of_two)
+game_of_two = two_players(x)
+print(game_of_two)
 
 # game_of_bots = bot_vs_bot(x)
 # print(game_of_bots)
 
-game_player_vs_bot = player_vs_bot(x)
-print(game_player_vs_bot)
+# game_player_vs_bot = player_vs_bot(x)
+# print(game_player_vs_bot)
 
